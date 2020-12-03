fdd714e626963f54f1fda6987be5ee7b3734d059
Harrison Booth <harrisonboothh@gmail.com>
2020-12-03 11:45:29 +0000

    add day 1 pt1 solution

---
+++ day_01/expenses_pt1.py
@@ -0,0 +1,7 @@
+str_expenses = open('input.txt', 'r').read().split('\n')
+expenses = [int(str_expense)
+            for str_expense in str_expenses if len(str_expense) > 0]
+
+for expense in expenses:
+    if 2020 - expense in expenses:
+        print(expense * (2020 - expense))
