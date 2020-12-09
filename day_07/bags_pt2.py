bd8bc9775e6ac51dfec0ec93b3d4dbfa334b0e6c
Harrison Booth <harrisonboothh@gmail.com>
2020-12-09 20:23:34 +0000

    add horrific solution for day 7 pt 2:wq

--- day_07/bags_pt2.py
+++ day_07/bags_pt2.py
@@ -3,30 +3,35 @@ import re
 bag_re = 's*,+\s*'
 bag_rules_pairs = [pair.split('s contain ') for pair in open(
     'input.txt', 'r').read().replace('s.', '').replace('.', '').split('\n')]
-# bag_rules_dicts = {outer: {'a': re.split('[1-9]\s{1}', inner_bag) for inner_bag in list(
-#     filter(lambda a: a != '', re.split(bag_re, inner)))} for [outer, inner] in bag_rules_pairs}
 
 bag_rules_lists = [[outer, list(
     filter(lambda a: a != '', re.split(bag_re, inner)))] for [outer, inner] in bag_rules_pairs]
 
+bag_rules_amounts = {outer: {bag_type: int(number) for [(number, bag_type)] in [
+    re.findall('([1-9])\s{1}(.*)', bag) for bag in inner if "no other bag" not in bag]}
+    for [outer, inner] in bag_rules_lists}
 
-# print(bag_rules_lists)
-# bag_rules_dicts = {outer: {'bag_type': '1' for inner_bag in re.split(
-#     '([1-9]\s){1}', inner_bag)} for [outer, inner] in bag_rules_lists}
-
-# [['bright purple bag', ['4 shiny chartreuse bag', '5 plaid lime bag', '2 dim magenta bag']]]
-
-bag_rules_dicts = [{outer: {bag_type: number for [(number, bag_type)] in [re.findall('([1-9])\s{1}(.*)', bag) for bag in inner if "no other bag" not in bag]}}
-                   for [outer, inner] in bag_rules_lists]
-
-print(bag_rules_dicts)
-
-# def outer_bag_count(bags, dict, containers=[]):
-#     new_containers = [key for (key, value) in dict.items() for bag in bags if bag in value]
-#     if (len(new_containers) < 1):
-#         return set(containers)
-#     containers.extend(new_containers)
-#     return outer_bag_count(new_containers, dict, containers)
+bag_rules_dicts = {outer: list(filter(lambda a: a != '', re.split(
+    bag_re, inner))) for [outer, inner] in bag_rules_pairs}
 
 
-# print(len(outer_bag_count(["shiny gold bag"], bag_rules_dicts)))
+# print(bag_rules_amounts)
+def outer_bag_count(bags, dict, containers=[]):
+    new_containers = [key for (key, value) in dict.items()
+                      for bag in bags if bag in value]
+    if (len(new_containers) < 1):
+        return set(containers)
+    containers.extend(new_containers)
+    return outer_bag_count(new_containers, dict, containers)
+
+
+def internal_bag_count(bags, dict, internal_bags=[]):
+    new_bags = [bag_type
+                for bag in bags for [bag_type, amount] in dict[bag].items() for i in range(amount)]
+    if len(new_bags) < 1:
+        return internal_bags
+    internal_bags.extend(new_bags)
+    return internal_bag_count(new_bags, dict, internal_bags)
+
+
+print(len(internal_bag_count(["shiny gold bag"], bag_rules_amounts)))
