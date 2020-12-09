122b29642dc65c986e61c2ea3bbce8aee72c79f1
Harrison Booth <harrisonboothh@gmail.com>
2020-12-09 11:53:42 +0000

    add day 5 pt 1

---
+++ day_05/seats_pt1.py
@@ -0,0 +1,20 @@
+def get_row(boarding_pass):
+    row_binary = boarding_pass[:7].replace("F", "0").replace("B", "1")
+    return int(int(row_binary, base=2))
+
+
+def get_column(boarding_pass):
+    column_binary = boarding_pass[7:].replace("L", "0").replace("R", "1")
+    return int(int(column_binary, base=2))
+
+
+def get_id(boarding_pass):
+    row = get_row(boarding_pass)
+    column = get_column(boarding_pass)
+    return (8 * row) + column
+
+
+passes = open('input.txt', 'r').read().split('\n')
+ids = [get_id(boarding_pass) for boarding_pass in passes]
+largest = max(ids)
+print(largest)
