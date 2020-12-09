1c57e7f5114344c3b2e95011459d3b969d91021a
Harrison Booth <harrisonboothh@gmail.com>
2020-12-09 19:29:02 +0000

    add day 7 pt2 bag mapping to dicts:wq

--- day_07/bags_pt2.py
+++ day_07/bags_pt2.py
@@ -1,8 +1,23 @@
 import re
 
 bag_re = 's*,+\s*'
-bag_rules_pairs = [pair.split('s contain ') for pair in open('input.txt', 'r').read().replace('s.', '').replace('.', '').split('\n')]
-bag_rules_dicts = {outer: {re.split('([1-9])\s{1}', inner_bag) for inner_bag in list(filter(lambda a: a != '', re.split(bag_re, inner)))} for [outer, inner] in bag_rules_pairs}
+bag_rules_pairs = [pair.split('s contain ') for pair in open(
+    'input.txt', 'r').read().replace('s.', '').replace('.', '').split('\n')]
+# bag_rules_dicts = {outer: {'a': re.split('[1-9]\s{1}', inner_bag) for inner_bag in list(
+#     filter(lambda a: a != '', re.split(bag_re, inner)))} for [outer, inner] in bag_rules_pairs}
+
+bag_rules_lists = [[outer, list(
+    filter(lambda a: a != '', re.split(bag_re, inner)))] for [outer, inner] in bag_rules_pairs]
+
+
+# print(bag_rules_lists)
+# bag_rules_dicts = {outer: {'bag_type': '1' for inner_bag in re.split(
+#     '([1-9]\s){1}', inner_bag)} for [outer, inner] in bag_rules_lists}
+
+# [['bright purple bag', ['4 shiny chartreuse bag', '5 plaid lime bag', '2 dim magenta bag']]]
+
+bag_rules_dicts = [{outer: {bag_type: number for [(number, bag_type)] in [re.findall('([1-9])\s{1}(.*)', bag) for bag in inner if "no other bag" not in bag]}}
+                   for [outer, inner] in bag_rules_lists]
 
 print(bag_rules_dicts)
 
